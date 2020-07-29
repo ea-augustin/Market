3f0ee33ff2b86ec25315f73ed6a8e9458739570e
Elijah <elidoom88@gmail.com>
2020-07-29 18:48:45 +0200

    Adding Component feature
    Taking  added component functionality to products view

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Product;
 
 class MainController extends Controller
 {
@@ -13,6 +14,8 @@ class MainController extends Controller
 
         //returing a view from a folder called products
         
-        return view('welcome');
+        return view('welcome')->with([
+            'products'=>Product::all(),
+        ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 @section( 'content')
-<h1>Welcome</h1>
-<p>Let's Begin</p>
+@empty($products)
+    <div class="alert alert-warning">
+        This list is empty!
+    </div> 
+    @else
+    <div class="row">
+    @foreach($products as $product)
+    <div class="col-3">
+    @include('components.product-card')
+    
+</div>
+    @endforeach
+</div>
+@endif
 @endsection
